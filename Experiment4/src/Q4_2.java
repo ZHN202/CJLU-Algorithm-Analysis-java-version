45e4324ae6e3a23ebc362b0f3b21d6a40f8986ce
ZHN202 <2823476292@qq.com>
2021-11-15 20:49:59 +0800

    finish all questions but still some little problem there, I think

--- Experiment4/src/Q4_2.java
+++ Experiment4/src/Q4_2.java
@@ -1,11 +1,32 @@
 public class Q4_2 {
+    public static void main(String[] args) {
+        AVLtree<Integer> aTree = new AVLtree<>();
+        aTree.insert(3);
+        aTree.insert(1);
+        aTree.insert(4);
+        aTree.insert(6);
+        aTree.insert(9);
+        aTree.insert(7);
+        aTree.insert(5);
+        aTree.insert(2);
+        System.out.println("leaves number:" + aTree.getLeavesNumber());
+        System.out.println("node number:" + aTree.getNodeNumber());
+        System.out.println("tree height:" + aTree.getHeight());
+        System.out.println("中序遍历：");
+        aTree.inorderTraversal();
+        System.out.println("后序遍历：");
+        aTree.postorderTraversal();
+        System.out.println("先序遍历：");
+        aTree.preorderTraversal();
+    }
+
 }
 
 class AVLtree<AnyType extends Comparable<? super AnyType>> {
-    AvlNode<AnyType> root;
-    private static final int ALLOWED_IMBALANCE = 1;
 
-    private class AvlNode<AnyType extends Comparable<? super AnyType>> {
+    private static final int ALLOWED_IMBALANCE = 1;
+    private int nodeNumber;
+    private static class AvlNode<AnyType extends Comparable<? super AnyType>> {
         AnyType data;
         AvlNode<AnyType> left;
         AvlNode<AnyType> right;
@@ -25,17 +46,57 @@ class AVLtree<AnyType extends Comparable<? super AnyType>> {
         }
     }
 
+    AvlNode<AnyType> root;
+
+    public AVLtree() {
+        root = null;
+        nodeNumber=0;
+    }
+
+    public void insert(AnyType data) {
+        root = insert(data, root);
+        nodeNumber++;
+    }
+
+    public int getNodeNumber(){
+        return nodeNumber;
+    }
+    public int getLeavesNumber(){
+        return leavesNumber(root);
+    }
+
+    public void preorderTraversal() {
+        if (nodeNumber == 0) System.out.println("Tree is empty!!!");
+        else preorder(root);
+        System.out.println();
+    }
+
+    public void inorderTraversal() {
+        if (nodeNumber == 0) System.out.println("Tree is empty!!!");
+        else inorder(root);
+        System.out.println();
+    }
+
+    public void postorderTraversal() {
+        if (nodeNumber == 0) System.out.println("Tree is empty!!!");
+        else postorder(root);
+        System.out.println();
+    }
+    public int getHeight(){
+        return treeHeight(root);
+    }
     private int height(AvlNode<AnyType> node) {
         return node == null ? -1 : node.height;
     }
 
     private AvlNode<AnyType> insert(AnyType data, AvlNode<AnyType> root) {
-        if (root == null) return new AvlNode<>(data);
+        if (root == null) return new AvlNode<>(data,null,null);
         int compareResult = data.compareTo(root.data);
         if (compareResult < 0) root.left = insert(data, root.left);
         else if (compareResult > 0) root.right = insert(data, root.right);
         return balance(root);
     }
+
     private AvlNode<AnyType> rotateWithLeftChild(AvlNode<AnyType> k2) {
         AvlNode<AnyType> k1 = k2.left;
         k2.left = k1.right;
@@ -44,27 +105,79 @@ class AVLtree<AnyType extends Comparable<? super AnyType>> {
         k1.height = Math.max(height(k1.left), k2.height) + 1;
         return k1;
     }
-
+    private AvlNode<AnyType> rotateWithRightChild(AvlNode<AnyType> k2){
+        AvlNode<AnyType> k1 = k2.right;
+        k2.right = k1.left;
+        k1.left = k2;
+        k2.height = Math.max(height(k2.right), height(k2.left)) + 1;
+        k1.height = Math.max(height(k1.right), k2.height) + 1;
+        return k1;
+    }
     private AvlNode<AnyType> doubleWithLeftChild(AvlNode<AnyType> k3) {
-        k3.left = rotateWithLeftChild(k3.left);
+        k3.left = rotateWithRightChild(k3.left);
         return rotateWithLeftChild(k3);
     }
+    private AvlNode<AnyType> doubleWithRightChild(AvlNode<AnyType> k3) {
+        k3.right = rotateWithLeftChild(k3.right);
+        return rotateWithRightChild(k3);
+    }
+
     private AvlNode<AnyType> balance(AvlNode<AnyType> root) {
         if (root == null) return root;
         if (height(root.left) - height(root.right) > ALLOWED_IMBALANCE)
-            if (height(root.left.left) >= height(root.right.right))
+            if (height(root.left.left) >= height(root.left.right))
                 root = rotateWithLeftChild(root);
             else
                 root = doubleWithLeftChild(root);
         if (height(root.right) - height(root.left) > ALLOWED_IMBALANCE)
-            if (height(root.right.right) >= height(root.left.left))
-                root = rotateWithLeftChild(root);
+            if (height(root.right.right) >= height(root.right.left))
+                root = rotateWithRightChild(root);
             else
-                root = doubleWithLeftChild(root);
+                root = doubleWithRightChild(root);
 
         root.height = Math.max(height(root.left), height(root.right)) + 1;
         return root;
     }
+    private int leavesNumber(AvlNode<AnyType> root) {
+        if (root == null) return 0;
+        if (root.right == null && root.left == null) return 1;
+        return (leavesNumber(root.left) + leavesNumber(root.right));
+    }
+    private void preorder(AvlNode<AnyType> root) {
+        if (root == null) System.out.println();
+        else {
+            inorder(root.left);
+            System.out.print(root.data + "->");
 
+            inorder(root.right);
+        }
+    }
+
+    private void inorder(AvlNode<AnyType> root) {
+        if (root != null) {
+            System.out.print(root.data + "->");
+            inorder(root.left);
+            inorder(root.right);
+        }
+    }
+
+    private void postorder(AvlNode<AnyType> root) {
+        if (root != null) {
+            inorder(root.right);
+            System.out.print(root.data + "->");
+            inorder(root.left);
+
+        }
+    }
+
+    private int treeHeight(AvlNode<AnyType> root) {
+        if (root == null) return 0;
+        else {
+            int leftHeight = treeHeight(root.left);
+            int rightHeight = treeHeight(root.right);
+            if (leftHeight > rightHeight) return (leftHeight + 1);
+            else return (rightHeight + 1);
+        }
+    }
 
 }
--- Experiment4/src/Q4_3.java
+++ Experiment4/src/Q4_3.java
@@ -1,2 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Q4_3 {
+    public static void main(String[] args) {
+        String filename = "/home/robert/桌面/Algorithm-Analysis-java-version/Experiment4/article.txt";
+
+        Map<String,Integer> map = new TreeMap<>();
+        try (Scanner sc = new Scanner(new FileReader(filename))) {
+            sc.useDelimiter(" ");  //分隔符
+
+            while (sc.hasNext()) {   //按分隔符读取字符串
+                String str = sc.next();
+                str = str.toLowerCase();
+                System.out.println(str);
+                str = str.replaceAll("[^a-z]", "");
+                if(map.containsKey(str)){
+                    map.replace(str,map.get(str)+1);
+                }
+                else{
+                    map.put(str,1);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        map.forEach((key,value)->System.out.println(key+":"+value));
+
+    }
 }
+
+
